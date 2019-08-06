e402bed783a1bda108a0f9dfe3bb1f9929217407
kanivets39 <47155060+kanivets39@users.noreply.github.com>
2019-08-06 16:23:27 +0300

    Add files via upload

---
+++ task4/src/test/java/com/company/FileServiceTest.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileServiceTest {
+
+    @Test
+    public void countering() {
+
+
+    }
+
+    @Test
+    public void fileoutput() {
+
+    }
+}
